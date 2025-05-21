1e6ae7e8ee46e3b2b630488e14a89d27a2e983c4
khifanmalik <almalikzulkhifan@gmail.com>
2025-05-21 15:47:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 selamat datang guys, follow @khifanmalik on ig")
+st.title("🎈 selamat datang guys")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.image(
