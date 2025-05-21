bb29d894075d806ac991d0760ab6bd22f8d1853b
khifanmalik <almalikzulkhifan@gmail.com>
2025-05-21 15:53:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 selamat datang guys")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-st.image(
+st.write("terimakasih banyak husain sudah membantu aku - zulkhifan")
+    
+st.image("IMG_0120.jpeg", width= 200)
